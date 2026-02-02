e381b7ee140319e513c18f68c24ad9b8c28d9f1f
János Perge <janos.perge@gmail.com>
2026-02-02 14:48:14 +0100

    change vite base url

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/vue-resume-template/',
-    plugins: [vue()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    }
-})
+  base: "/resume/",
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import",
+        ],
+      },
+    },
+  },
+});
